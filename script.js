621548586cdf7b421212742e30013aea333d03ca
James de Klerk <jamesdeklerk@gmail.com>
2022-09-04 21:00:50 +0200

    Added the use of the mediaSession to provide better play and pause functionality

--- script.js
+++ script.js
@@ -12,8 +12,12 @@
 // https://web.dev/media-session/
 
 let volumeSliderElement = document.getElementById("volumeSlider");
+let playPauseButton = document.getElementById("playPauseButton");
 let audioContext = undefined; // Leave as undefined, until user gesture is performed
 let gainNode = undefined;
+let audioElement = document.getElementById("audioElement");
+audioElement.volume = 1;
+audioElement.loop = true;
 
 function playAudioBuffer(audioBuffer, loop) {
     // Create a new audio source node (AudioBufferSourceNode)
@@ -36,25 +40,43 @@ function playAudioBuffer(audioBuffer, loop) {
 }
 
 async function playPauseClick() {
-    if (audioContext !== undefined) {
-        if (audioContext.state === "running") {
-            audioContext.suspend();
-            return;
-        }
+    if (audioContext === undefined) {
+        audioContext = new AudioContext();
 
-        if (audioContext.state === "suspended") {
-            audioContext.resume();
-            return;
-        }
+        var audioBuffer = await getAudioAsync("resources/rain-from-room-loop-smallest.ogg", audioContext);
+    
+        playAudioBuffer(audioBuffer, true);
+
+        await initMediaSession();
 
         return;
     }
 
-    audioContext = new AudioContext();
+    if (audioContext.state === "running") {
+        await pauseAudio();
+        return;
+    }
 
-    var audioBuffer = await getAudioAsync("resources/rain-from-room-loop-smallest.ogg", audioContext);
+    if (audioContext.state === "suspended") {
+        await playAudio();
+        return;
+    }
+}
 
-    playAudioBuffer(audioBuffer, true);
+async function playAudio() {
+    console.log("playAudio");
+    await audioContext.resume();
+    await audioElement.play();
+    navigator.mediaSession.playbackState = "playing";
+    playPauseButton.innerHTML = "Pause";
+}
+
+async function pauseAudio() {
+    console.log("pauseAudio");
+    await audioContext.suspend();
+    audioElement.pause();
+    navigator.mediaSession.playbackState = "paused";
+    playPauseButton.innerHTML = "Play";
 }
 
 /**
@@ -78,8 +100,51 @@ volumeSliderElement.addEventListener("input", () => {
     }
 });
 
-document.addEventListener('keyup', (event) => {
-    if (event.key === "MediaPlayPause") {
-        playPauseClick();
-    }
-});
+
+async function initMediaSession() {
+    await playAudio();
+
+    navigator.mediaSession.metadata = new MediaMetadata({
+        title: 'Rain',
+        artist: 'Soundscape',
+        album: 'Nature',
+        artwork: [
+            { src: 'https://via.placeholder.com/96', sizes: '96x96', type: 'image/png' },
+            { src: 'https://via.placeholder.com/128', sizes: '128x128', type: 'image/png' },
+            { src: 'https://via.placeholder.com/192', sizes: '192x192', type: 'image/png' },
+            { src: 'https://via.placeholder.com/256', sizes: '256x256', type: 'image/png' },
+            { src: 'https://via.placeholder.com/384', sizes: '384x384', type: 'image/png' },
+            { src: 'https://via.placeholder.com/512', sizes: '512x512', type: 'image/png' },
+        ]
+    });
+    
+    navigator.mediaSession.setActionHandler("play", async () => {
+        console.log("mediaSession - play");
+        await playAudio();
+    });
+    navigator.mediaSession.setActionHandler("pause", async () => {
+        console.log("mediaSession - pause");
+        await pauseAudio();
+    });
+    navigator.mediaSession.setActionHandler("previoustrack", async () => {
+        console.log("mediaSession - previoustrack");
+    });
+    navigator.mediaSession.setActionHandler("nexttrack", async () => {
+        console.log("mediaSession - nexttrack");
+    });
+    navigator.mediaSession.setActionHandler("stop", async () => {
+        console.log("mediaSession - stop");
+        await initMediaSession();
+        await pauseAudio();
+    });
+
+    // Sometimes the mediaSession events don't fire, this is backup
+    audioElement.addEventListener("play", async () => {
+        console.log("audioElement - play");
+        await playAudio();
+    });
+    audioElement.addEventListener("pause", async () => {
+        console.log("audioElement - pause");
+        await pauseAudio();
+    });
+}
