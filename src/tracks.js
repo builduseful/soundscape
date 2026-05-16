73048f4ddd7e9f56c1722e49f28c77c6d54bf5a8
James de Klerk <jamesdeklerk@gmail.com>
2026-05-16 20:03:10 +0200

    Improved track order

--- src/tracks.js
+++ src/tracks.js
@@ -17,7 +17,7 @@ export const tracks = [
     { title: "The Open Road", url: "resources/soundscapes/open-road-loop.opus", mime: OGG_OPUS_MIME },
 
     // Noises
-    { title: "White Noise", url: "resources/soundscapes/white-noise-loop.opus", mime: OGG_OPUS_MIME },
-    { title: "Pink Noise", url: "resources/soundscapes/pink-noise-loop.opus", mime: OGG_OPUS_MIME },
     { title: "Brown Noise", url: "resources/soundscapes/brown-noise-loop.opus", mime: OGG_OPUS_MIME },
+    { title: "Pink Noise", url: "resources/soundscapes/pink-noise-loop.opus", mime: OGG_OPUS_MIME },
+    { title: "White Noise", url: "resources/soundscapes/white-noise-loop.opus", mime: OGG_OPUS_MIME },
 ];
