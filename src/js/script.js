1a9f678ea210182ae2754855baf233791642d1ff
James de Klerk <jamesdeklerk@gmail.com>
2026-07-25 20:42:08 +0200

    Bump version to 1.4.0
    Structural refactor consolidated deployable files under src/.

--- src/js/script.js
+++ src/js/script.js
@@ -25,7 +25,7 @@ import {
     saveThemePreference,
 } from "./theme-utils.js";
 import { tracks, trackSlug } from "./tracks.js";
-const VERSION = "1.3.0";
+const VERSION = "1.4.0";
 
 const PLAY_LABEL = "Play";
 const PAUSE_LABEL = "Pause";
